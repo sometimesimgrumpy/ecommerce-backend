7dc5fd82e5d906e29829a38359e837d357ad34fc
Brianna <briannaodell1437@gmail.com>
2022-08-24 15:18:38 -0700

    create model files to the homework specification

--- models/index.js
+++ models/index.js
@@ -1,16 +1,27 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+  // TODO: check this later for correct on delete
+  onDelete: "CASCADE",
+});
 
+// https://sequelize.org/api/v6/class/src/associations/belongs-to-many.js~belongstomany
 // Products belongToMany Tags (through ProductTag)
+Product.belongToMany(Tag, { through: ProductTag });
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongToMany(Product, { through: ProductTag });
 
 module.exports = {
   Product,
